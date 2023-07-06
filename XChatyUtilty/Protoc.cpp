7a834d6f0fd982d33c33e45e2c21c0283b079f81
CICSilver <1038516368@qq.com>
2023-07-06 10:19:53 +0800

    新增通信协议

---
+++ XChatyUtilty/Protoc.cpp
@@ -0,0 +1,14 @@
+#include "Protoc.h"
+
+LoginRequest::LoginRequest(const char* time, quint16 _check_sum)
+{
+}
+
+QByteArray LoginRequest::Serrialize(const char* msg)
+{
+	return QByteArray();
+}
+
+const char* LoginRequest::DeSerrialize(const QByteArray& ba)
+{
+}
